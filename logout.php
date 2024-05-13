bfa1a5fa2da7faf7604b6cbbb19f33dbdfd43c83
Paleksz <96802620+Hardyzom@users.noreply.github.com>
2024-05-13 12:25:58 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 $backgroundColor = "#2076aa"; 
 
-$logoutMessage = "You have been successfully logged out. Please log in again!"; 
+$logoutMessage = "Sikeresen kijelentkezve!"; 
 
 header("Refresh: 0; url=index.php?color=" . urlencode($backgroundColor) . "&response=" . urlencode($logoutMessage));
 exit;
